685f2320aa9bbbdda7b2c8eec1e66eac4fb2deb0
Kielx <kiel66@gmail.com>
2020-08-10 10:30:42 +0200

    Move controllers to main directory

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,4 +1,4 @@
-const Post = require("../../models/post");
+const Post = require("../models/post");
 const markdown = require("marked");
 
 module.exports = {
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 router = express.Router();
 
-const postsController = require("./controllers/postsController");
+const postsController = require("../controllers/postsController");
 
 router.post("/posts/change/", postsController.updatePost);
 router.get("/posts/change/", postsController.updatePostRoute);
