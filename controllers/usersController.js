32a64cb110529c78d18df32d9668152133a6ac58
Kielx <kiel66@gmail.com>
2020-08-18 20:30:52 +0200

    add new register page as register2, lay base for express-validator

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const { body, validationResult } = require("express-validator");
 
 module.exports = {
   displayRegister: (req, res) => {
@@ -11,6 +12,63 @@ module.exports = {
           username: req.flash("username"),
           email: req.flash("email"),
         });
+      });
+  },
+
+  displayRegister2: (req, res) => {
+    User.find({})
+      .select({})
+      .exec(function (err, result) {
+        res.render("register2", {
+          users: result,
+          ErrorMessages: req.flash("errorMessages"),
+          username: req.flash("username"),
+          email: req.flash("email"),
+          passwords: req.flash("passwords"),
+        });
+      });
+  },
+
+  register2: (req, res) => {
+    /*     if (req.body.Password !== req.body.Password2) {
+      req.flash("passwords", "Passwords do not match!");
+      res.redirect("/users/register2");
+    } */
+
+    const errors = validationResult(req);
+    console.log(req.body);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).jsonp(errors.array());
+    }
+
+    errorMessages = [];
+    User.create({
+      username: req.body.Username,
+      password: req.body.Password,
+      email: req.body.email,
+    })
+      .then((newUser) => {
+        res.redirect("/users/login");
+      })
+      .catch((err) => {
+        if (err.errors["username"]) {
+          errorMessages.push(
+            `Username you entered ${err.errors["username"].message}`
+          );
+        }
+        if (err.errors["password"]) {
+          errorMessages.push(`Password ${err.errors["password"].message}`);
+        }
+        if (err.errors["email"]) {
+          errorMessages.push(
+            `E-mail you entered ${err.errors["email"].message}`
+          );
+        }
+        req.flash("username", req.body.Username);
+        req.flash("email", req.body.email);
+        req.flash("errorMessages", errorMessages);
+        res.redirect("/users/register2");
       });
   },
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,21 @@
 const express = require("express");
-
+const { body, check, validationResult } = require("express-validator");
 router = express.Router();
 
 const postsController = require("../controllers/postsController");
 const usersController = require("../controllers/usersController");
 
+router.post(
+  "/users/register2",
+  [
+    check("Password2", "Passwords do not match").custom(
+      (value, { req }) => value === req.body.Password
+    ),
+  ],
+  usersController.register2
+);
+
+router.get("/users/register2", usersController.displayRegister2);
 router.get("/users/register", usersController.displayRegister);
 router.post("/users/register", usersController.register);
 router.post("/posts/change/", postsController.updatePost);
